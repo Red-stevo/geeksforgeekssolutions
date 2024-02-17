008bed8d72907b2d5d4add4625082882cae1c0d6
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-17 21:50:00 +0300

    resting for the day

--- SubArrayWithGivenSum/src/Main.java
+++ SubArrayWithGivenSum/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution2.subarraySum(new int[]{1,2,3,7,5}, 5,12));
+        System.out.println(Solution2.subarraySum(new int[]{0}, 2,1));
     }
 }
