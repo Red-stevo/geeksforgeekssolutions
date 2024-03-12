f7afbaf766631328f6a26feb0e10cbec83c91f4a
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-12 06:14:04 +0300

    modified

--- KadanesAlgorithm/src/KanadesAlgorithmImpl.java
+++ KadanesAlgorithm/src/KanadesAlgorithmImpl.java
@@ -17,4 +17,33 @@ public class KanadesAlgorithmImpl implements IKadanesAlgorithm {
 
         return MAX_VALUE; //autoboxing of the wrapper class.
     }
+
+
+    /*Modifying the method above to get the pointer at the start and the end of the max_sum_sub_array*/
+
+    public int[] maxSubArraySum(int[] arr){
+
+        long maxSum = Long.MIN_VALUE, sum = 0L;
+        int start = 0,end  = 0, startPointer = 0;
+
+
+        for (int i = 0 ; i < arr.length ; i++) {
+            if(sum == 0L)
+                start = i;
+
+            sum += arr[i];
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                startPointer = start;
+                end = i;
+            }
+
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return new int[]{startPointer, end};
+    }
 }
--- KadanesAlgorithm/src/Main.java
+++ KadanesAlgorithm/src/Main.java
@@ -3,11 +3,20 @@ public class Main {
 
         IKadanesAlgorithm kadanesAlgorithm = new KanadesAlgorithmImpl();
 
-        int[] array = {8,-4,12,-3,-1};
+        int[] array = {-3,8,-4,12,-3,-1};
         int n = array.length;
 
         Long maxSum = kadanesAlgorithm.maxSubArraySum(array, n);
 
         System.out.println(maxSum);
+
+
+        KanadesAlgorithmImpl kanadesAlgorithm = new KanadesAlgorithmImpl();
+
+        int[] subArray = kanadesAlgorithm.maxSubArraySum(array);
+
+        for(int i : subArray){
+            System.out.print(i + "\t");
+        }
     }
 }
